672fd30e1912cafece93d974de76ecfab6123f45
Milson <milson7017@yandex.ru>
2023-06-07 21:45:49 +0300

    dz ok

--- test_qaguru_dz2.py
+++ test_qaguru_dz2.py
@@ -6,3 +6,8 @@ def test_qaguru_auth():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+def test_dz_no_result():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('gewgwegewgee').press_enter()
+    browser.element('[class="card-section"]').should(have.text('По запросу gewgwegewgee ничего не найдено. '))

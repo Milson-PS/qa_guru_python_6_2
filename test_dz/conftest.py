137f1fc24d394b5e392f3d923ec474ae20fb14d6
Milson <milson7017@yandex.ru>
2023-06-08 13:52:17 +0300

    reformat code

--- test_dz/conftest.py
+++ test_dz/conftest.py
@@ -1,10 +1,10 @@
 import pytest
 from selene.support.shared import config
 
-@pytest.fixture(autouse = True)
+
+@pytest.fixture(autouse=True)
 def setup_browser():
     config.timeout = 10
     config.window_width = 800
     config.window_height = 600
     config.base_url = 'https://www.google.com/ncr'
-
--- test_dz/test_qaguru_dz2.py
+++ test_dz/test_qaguru_dz2.py
@@ -1,11 +1,12 @@
-
 from selene.support.shared import browser
 from selene import be, have
+
 
 def test_qaguru_auth():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
 
 def test_dz_no_result():
     browser.open('https://google.com')
